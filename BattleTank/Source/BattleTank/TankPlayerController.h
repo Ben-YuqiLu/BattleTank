0ebfdc98462f4709dd47dfd98140a74011826d95
Yuqi_Lu <lu_yuqi@outlook.com>
2020-08-13 22:16:40 +0800

    Add Tick() to TankPlayerController

--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -17,7 +17,9 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 	
 public:
 	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
 
 	ATank* GetControlledTank() const;
 private:
+	void AimTowardsCrosshair();
 };
