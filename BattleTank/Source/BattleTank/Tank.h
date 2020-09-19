f531f0e0a8e08cee27576527fa963e9f2badb836
Yuqi_Lu <lu_yuqi@outlook.com>
2020-09-19 18:33:45 +0800

    SuggestProjectileVelocity()

--- BattleTank/Source/BattleTank/Tank.h
+++ BattleTank/Source/BattleTank/Tank.h
@@ -17,23 +17,24 @@ public:
 	// Sets default values for this pawn's properties
 	ATank();
 
+	void AimAt(FVector HitLocation);
+
+	UFUNCTION(BlueprintCallable, Category = Setup)
+	void SetBarrelReference(UStaticMeshComponent* BarrelToSet);
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
+private:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-
-public:
-	void AimAt(FVector HitLocation);
-
-	UFUNCTION(BlueprintCallable, Category = Setup)
-	void SetBarrelReference(UStaticMeshComponent* BarrelToSet);
+	UPROPERTY(EditAnywhere, Category = "Firing")
+	float LaunchSpeed = 100000; //TODO find sensible default value
 
 protected:
 	UTankAimingComponent* TankAimingComponent = nullptr;
